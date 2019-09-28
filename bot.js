6297289838d2c2b1117bdc70e6072242c8aec145
Cameron Knickerbocker <48843726+cameron1752@users.noreply.github.com>
2019-09-28 12:53:29 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -11,19 +11,52 @@ bot.on('ready', () => {
     console.log('This bot is online stupid!');
 })
 
+// counter variable
 var i;
 
 // message processing for toxicity
 bot.on('message', msg => {
-    
+    // holds message content for testing
+    // var sup = message.content;
+    // prints message content for testing
+    // console.log(sup)
+    // array holding the split message for processing
     let args = msg.content.substring(prefix.length).split(" ");
-
-    switch(args[0]){
-        case 'toxic':
-            for ( i = 0; i < 10; i++){
-                msg.channel.send(args[1] + ' ur bad');
-            }
-            break;
+    //to catch the username and get message
+    if (args[1] != null){
+        // variable to hold username length to properly save the rest of the message
+        var len = args[1].length;
+        // holds the rest of the message
+        let stuff = msg.content.substring(7 + len);
+        // holds the user being @'d user name
+        var name = args[1];
+        // switch for more functionality
+        switch(args[0]){
+            case 'toxic':
+                // checks for ultra toxic
+                if (args[1] == '!toxic' || args[1] == 'toxic'){
+                    msg.channel.send("nice try shitter " + msg.author);
+                    break;
+                }
+                // prints the sent message 10 times
+                for ( i = 0; i < 10; i++){
+                    msg.channel.send(args[1] + ' ' + stuff);
+                }
+                args.length = 0;
+                break;
+            case 'flush':
+                var member = msg.mentions.members.first();
+                // ban
+                member.kick().then((member) => {
+                    // Successmessage
+                    msg.channel.send(":wave: " + member.displayName + " has been successfully banned :point_right: ");
+                }).catch(() => {
+                        // Failmessage
+                    msg.channel.send("Access Denied");
+                });
+                break; 
+        }
     }
 })
+
 bot.login(token);
